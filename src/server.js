601bd29b83eaadec692e90c30b103ee3ef69cd75
anphuc0311 <dangdinhanphuc@gmail.com>
2025-11-01 01:11:32 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path')
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
+
+const app = express();
+const port = process.env.PORT;
+const hostname = process.env.HOST_NAME;
+
+console.log('Hostname from .env:', port);
+
+// Configure the view engine
+configViewEngine(app);
+
+// Routes
+app.use('/', webRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
