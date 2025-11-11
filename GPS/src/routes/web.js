d7d7995762070f02d78e20836cc0892b3d3507b7
anphuc0311 <dangdinhanphuc@gmail.com>
2025-11-11 19:40:43 +0700

    delete

--- GPS/src/routes/web.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const {getHomePage, test} = require('../controllers/homeController');
-
-
-route.get('/', getHomePage);
-route.get('/test', test);
-
-module.exports = route; //export default
